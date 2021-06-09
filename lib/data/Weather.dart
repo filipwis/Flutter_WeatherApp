32331b1622d41779e4009d753e9045a7213c944d
Filip Wisniowski <wisniowskifilip@gmail.com>
2021-06-09 17:25:25 +0200

    Removed the time picker from main screen. Changed API json from hourly to daily.

--- lib/data/Weather.dart
+++ lib/data/Weather.dart
@@ -1,16 +1,11 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-
 class Weather {
   String cityName;
-  DateTime date;
   String temperature;
   String icon;
   List<Weather>? forecast;
 
   Weather({
     required this.cityName,
-    required this.date,
     required this.temperature,
     required this.icon,
     this.forecast,
@@ -18,40 +13,26 @@ class Weather {
 
   factory Weather.fromJson(Map<String, dynamic> json) {
     String cityName = json["city"]["name"];
-    String currentTemp = "";
     String temp = "";
     List<Weather> weatherForecast = [];
     for (final item in json["list"]) {
-      if (item["main"]["temp"].toString().contains(".")) {
-        temp = item["main"]["temp"]
+      if (item["temp"]["day"].toString().contains(".")) {
+        temp = item["temp"]["day"]
             .toString()
-            .substring(0, item["main"]["temp"].toString().indexOf("."));
+            .substring(0, item["temp"]["day"].toString().indexOf("."));
       } else {
-        temp = item["main"]["temp"].toString();
+        temp = item["temp"]["day"].toString();
       }
       weatherForecast.add(Weather(
           cityName: cityName,
-          date: DateTime.parse(item["dt_txt"]),
           temperature: temp,
           icon: item["weather"][0]["icon"]));
     }
-    if (json["list"][0]["main"]["temp"].toString().contains(".")) {
-      currentTemp = json["list"][0]["main"]["temp"].toString().substring(
-          0, json["list"][0]["main"]["temp"].toString().indexOf("."));
-    } else {
-      currentTemp = json["list"][0]["main"]["temp"].toString();
-    }
     return Weather(
         cityName: json["city"]["name"],
-        date: DateTime.parse(json["list"][0]["dt_txt"]),
-        temperature: currentTemp,
-        icon: json["list"][0]["weather"][0]["icon"],
+        temperature: weatherForecast[0].temperature,
+        icon: weatherForecast[0].icon,
         forecast: weatherForecast);
-  }
-
-  Weather getForecastTemp(int day, int hour) {
-    return this.forecast!.firstWhere(
-        (element) => element.date.day == day && element.date.hour == hour);
   }
 }
 
